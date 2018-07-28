fb314f8b64889d72001dfb35e46ae371954481c1
www-data <www-data@kckern.info>
2018-07-28 07:15:53 +0000

    Build  1126: http://jenkins.kckern.info/job/Isaiah_CI/1126/ /console

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RouterShell from './Router';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<RouterShell />, document.getElementById('root'));
-registerServiceWorker();
+
