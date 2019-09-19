281c62f1fde627759c5816456b0b7f5e8666e6fb
KC Kern <kc@kckern.com>
2019-09-19 20:37:12 +0900

    Build  1431: http://jenkins.kckern.info/job/Isaiah_CI/1431/console

--- public/electron.js
+++ public/electron.js
@@ -1,7 +1,8 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-
+const Menu = electron.Menu;
+const isDev = require("electron-is-dev");
 const path = require('path');
 
 let mainWindow;
@@ -9,15 +10,43 @@ let mainWindow;
 function createWindow() {
     mainWindow = new BrowserWindow({
         width: 1920, height: 1200,
+        title: "Isaiah Explorer",
         webPreferences: {
-            devTools: false
+            devTools: true
         }
     });
-    mainWindow.loadURL(`file://${path.join(__dirname, '../build/index.html')}`);
+    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
     mainWindow.on('closed', () => mainWindow = null);
+
 }
 
-app.on('ready', createWindow);
+app.on('ready', function () {
+
+    createWindow();
+    console.log(app);
+    const template = [
+        {
+            label: 'File', submenu: [
+                { label: 'Quit', role: 'quit' }
+            ]
+        },
+        {
+            label: 'Structure', submenu: [
+                { label: 'Whole', role: 'file-new' },
+                { type: 'separator' },
+                { label: 'Quit', role: 'quit' }
+            ]
+        },
+        {
+            label: 'Outline', submenu: [
+                { label: 'Whole', role: 'file-new' },
+                { type: 'separator' },
+                { label: 'Quit', role: 'quit' }
+            ]
+        }
+    ];
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+});
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
